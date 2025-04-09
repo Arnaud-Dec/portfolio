421fcf2077946b6e295f38b9db0587ae47e33527
Arnaud <arnaud.decourt@etu.univ-lyon1.fr>
2025-04-09 23:35:06 +0200

    fix : test fix en prod pour pp

--- components/profile-image.tsx
+++ components/profile-image.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
+import { fixAssetPath } from "@/lib/fixAssetPath"
 
 export default function ProfileImage() {
   return (
     <div className="relative w-32 h-32 md:w-40 md:h-40 mx-auto mb-8">
       <div className="absolute inset-0 rounded-full bg-gradient-to-br from-white/20 to-white/5 animate-pulse"></div>
       <Image
-        src="assets/perso/pp.jpg?height=160&width=160"
+        src={fixAssetPath("assets/perso/pp.jpg?height=160&width=160")}
         alt="Arnaud Decourt"
         width={160}
         height={160}
